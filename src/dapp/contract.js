29c87f2b7102b4cea2d9503b8c8def65878cc710
Manar Altamimi <manar.altamimi@hotmail.com>
2019-08-06 22:12:40 +0100

    v2

---
+++ src/dapp/contract.js
@@ -0,0 +1,65 @@
+import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import Config from './config.json';
+import Web3 from 'web3';
+
+export default class Contract {
+    constructor(network, callback) {
+
+        let config = Config[network];
+        this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+        this.initialize(callback);
+        this.owner = null;
+        this.airlines = {};
+        this.passengers = {};
+        this.airlineNames = ['Air1','Air2','Air3','Air4','AirA'];
+        this.passengerNames = ['P1','P2','P3','P4','PA5'];
+        this.flights = {
+            'Air1':['FL1','FL2','FL3'],
+            'Air2':['AL1','AL2','AL3'],
+            'Air3':['BL1','BL2','BL3'],
+            'Air4':['CL1','CL2','CL3'],
+            'Air5':['DL1','DL2','DL3'],
+        }
+    }
+
+    initialize(callback) {
+        this.web3.eth.getAccounts((error, accts) => {
+
+            this.owner = accts[0];
+
+            let counter = 1;
+
+            while(this.airlines.length < 5) {
+                this.airlines.push(accts[counter++]);
+            }
+
+            while(this.passengers.length < 5) {
+                this.passengers.push(accts[counter++]);
+            }
+
+            callback();
+        });
+    }
+
+    isOperational(callback) {
+       let self = this;
+       self.flightSuretyApp.methods
+            .isOperational()
+            .call({ from: self.owner}, callback);
+    }
+
+    fetchFlightStatus(flight, callback) {
+        let self = this;
+        let payload = {
+            airline: self.airlines[0],
+            flight: flight,
+            timestamp: Math.floor(Date.now() / 1000)
+        }
+        self.flightSuretyApp.methods
+            .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
+            .send({ from: self.owner}, (error, result) => {
+                callback(error, payload);
+            });
+    }
+}
